1a133ba6003a3b039f01ca628e67ae41bcbb9181
Jose Manuel Campos Soberano <jmcsoberano@gmail.com>
2020-05-24 18:26:20 -0500

    Create applications posts. Visualization imagen list

--- globalgram/urls.py
+++ globalgram/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from globalgram import views as local_views
+from posts import views as posts_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello_world', local_views.hello_world),
+
+    path('posts/', posts_views.list_posts),
 ]
---
+++ posts/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from datetime import datetime
+
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+# Create your views here.
+def list_posts(request):
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
