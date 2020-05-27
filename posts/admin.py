5421a1afaaaaba5976f75361ca4c8bcc0a0be5b6
Jose Manuel Campos Soberano <jmcsoberano@gmail.com>
2020-05-27 11:35:28 -0500

    Add Signup

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from posts.models import Post
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+
+    list_display = ('id', 'user', 'title', 'photo')
+    search_fields = ('title', 'user__username', 'user__email')
+    list_filter = ('created', 'modified')
--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.models import User
+from users.models import Profile
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+from django.db.utils import IntegrityError
 
 
 # Create your views here.
@@ -16,6 +21,33 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username or password'})
 
     return render(request, 'users/login.html')
+
+def signup(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
 
 @login_required
 def logout_view(request):
