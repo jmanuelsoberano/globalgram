c8ef27f919b65ad11c8dd62f3bff91d42571d504
Jose Manuel Campos Soberano <jmcsoberano@gmail.com>
2020-06-14 12:34:05 -0500

    Refactor Post

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from posts.forms import PostForm
 
@@ -22,22 +21,13 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'post'
 
 
-@login_required
-def create_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
+class CreatePostView(LoginRequiredMixin, CreateView):
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
 
-    return render(
-        request=request,
-        template_name='posts/new.html',
-        context={
-            'form': form,
-            'user': request.user,
-            'profile': request.user.profile
-        }
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
+        return context
